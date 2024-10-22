b99a2d0335174fe53e2e3cbd7d12ad6a8243e5e9
Michael J <30031129@cc.peralta.edu>
2024-10-21 21:53:47 -0700

    code now correctly validates customer number

--- getCustomerNumber.cpp
+++ getCustomerNumber.cpp
@@ -1,4 +1,5 @@
 #include "./getCustomerNumber.hpp"
+#include "./validateNumber.hpp"
 #include <iostream>
 
 using namespace std;
@@ -6,10 +7,17 @@ using namespace std;
 namespace customer {
 	void getCustomerNumber() {
 		string customerNumber;
-		cout << "what is your customer number (should be two letters and four digits)?" << endl;
-		
-		cin >> customerNumber;
-	
+		while(true){
+			cout << "what is your customer number (should be two letters and four digits)?" << endl;
+			cin >> customerNumber;
+			cout << "validating number..." << endl;
+			if (validate::validateNumber(customerNumber)) {
+				cout << "\nvalidation completed! Your customer number is: " << customerNumber << endl;
+				break;
+			}
+			cout << "Error: Customer number format incorrect." << "\n\n";
+
+		}
 		
 	}
 }
